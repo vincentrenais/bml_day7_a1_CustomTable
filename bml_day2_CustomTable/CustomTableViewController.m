dee7069e8e5949943bf0b500164e9a4d38a15a13
Vincent Renais <vincentrenais@gmail.com>
2015-04-16 13:58:41 -0400

    Chapter 7 & 8 finished

--- bml_day2_CustomTable/CustomTableViewController.m
+++ bml_day2_CustomTable/CustomTableViewController.m
@@ -24,13 +24,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"recipes" ofType:@"plist"];
+    NSDictionary *dict = [[NSDictionary alloc] initWithContentsOfFile:path];
+    recipeNames = [dict objectForKey:@"Name"];
+    recipeImages = [dict objectForKey:@"Image"];
+    recipePrepTime = [dict objectForKey:@"PrepTime"];
     
-    recipeNames = [NSMutableArray arrayWithObjects: @"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
-    
-    
-    recipePrepTime = [NSMutableArray arrayWithObjects: @"30Min", @"36Min", @"54Min", @"25Min", @"54Min", @"25Min", @"36Min", @"30Min", @"54Min", @"30Min", @"54Min", @"36Min", @"54Min", @"36Min", @"54Min", @"36Min", nil];
-    
-    recipeImages = [NSMutableArray arrayWithObjects: @"egg_benedict.jpg", @"mushroom_risotto.jpg", @"full_breakfast.jpg", @"hamburger.jpg", @"ham_and_egg_sandwich.jpg", @"creme_brelee.jpg", @"white_chocolate_donut.jpg", @"starbucks_coffee.jpg", @"vegetable_curry.jpg", @"instant_noodle_with_egg.jpg", @"noodle_with_bbq_pork.jpg", @"japanese_noodle_with_pork.jpg", @"green_tea.jpg", @"thai_shrimp_cake.jpg", @"angry_birds_cake.jpg", @"ham_and_cheese_panini.jpg", nil];
+//    recipeNames = [NSMutableArray arrayWithObjects: @"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
+//    
+//    
+//    recipePrepTime = [NSMutableArray arrayWithObjects: @"30Min", @"36Min", @"54Min", @"25Min", @"54Min", @"25Min", @"36Min", @"30Min", @"54Min", @"30Min", @"54Min", @"36Min", @"54Min", @"36Min", @"54Min", @"36Min", nil];
+//    
+//    recipeImages = [NSMutableArray arrayWithObjects: @"egg_benedict.jpg", @"mushroom_risotto.jpg", @"full_breakfast.jpg", @"hamburger.jpg", @"ham_and_egg_sandwich.jpg", @"creme_brelee.jpg", @"white_chocolate_donut.jpg", @"starbucks_coffee.jpg", @"vegetable_curry.jpg", @"instant_noodle_with_egg.jpg", @"noodle_with_bbq_pork.jpg", @"japanese_noodle_with_pork.jpg", @"green_tea.jpg", @"thai_shrimp_cake.jpg", @"angry_birds_cake.jpg", @"ham_and_cheese_panini.jpg", nil];
 
 }
 

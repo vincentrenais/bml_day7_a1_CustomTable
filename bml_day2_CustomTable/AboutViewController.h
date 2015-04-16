1b645f77b16984b7e6b8cd37f491590e60219daf
Vincent Renais <vincentrenais@gmail.com>
2015-04-16 17:19:10 -0400

    Add a web view in Storyboard

---
+++ bml_day2_CustomTable/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  bml_day2_CustomTable
+//
+//  Created by Vincent Renais on 2015-04-16.
+//  Copyright (c) 2015 Vincent Renais. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
